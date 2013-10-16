95fe5f4bb585b37a2b5fadb905ce9bf16012fae7
QuillyT <QuillyT@gmail.com>
2013-10-15 18:57:00 -0600

    Finished Refactoring. Added IdeaStore class and layouts to cut down on boilerplate.

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require './idea'
+require './idea_store'
 require 'bundler'
 Bundler.require
 
@@ -6,40 +7,35 @@ class IdeaBoxApp < Sinatra::Base
 
   set :method_override, true
 
-  not_found do
-    erb  :error
-  end
-
   configure :development do
     register Sinatra::Reloader
   end
 
+  not_found do
+    erb  :error
+  end
+
   get '/' do
-    erb :index, locals: {ideas: Idea.all}
+    erb :index, locals: {ideas: IdeaStore.all, idea: Idea.new}
   end
 
   post '/' do
-    idea = Idea.new(params['idea_title'], params['idea_description'])
-    idea.save
+    IdeaStore.create(params[:idea])
     redirect '/'
   end
 
   delete '/:id' do |id|
-    Idea.delete(id.to_i)
+    IdeaStore.delete(id.to_i)
     redirect '/'
   end
 
   get '/:id/edit' do |id|
-    idea = Idea.find(id.to_i)
+    idea = IdeaStore.find(id.to_i)
     erb :edit, locals: {id: id, idea: idea}
   end
 
   put '/:id' do |id|
-    data = {
-      :title => params['idea_title'],
-      :description => params['idea_description']
-    }
-    Idea.update(id.to_i, data)
+    IdeaStore.update(id.to_i, params[:idea])
     redirect '/'
   end
 
